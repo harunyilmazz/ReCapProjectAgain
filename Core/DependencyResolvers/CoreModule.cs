8cf4b803bfe817bcdab52205b24e382c198ea6d0
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-03-25 01:56:45 +0300

    MemoryCache altyapısı eklendi

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,9 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
         }
     }
 }
